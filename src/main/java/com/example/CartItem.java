72299da2b98045272913a512e194480397a30273
Raffi <raffi9998@gmail.com>
2025-01-29 15:14:54 +0100

    Cart item added new metods

--- src/main/java/com/example/CartItem.java
+++ src/main/java/com/example/CartItem.java
@@ -1,7 +1,6 @@
 package com.example;
 
 class CartItem {
-
     private String name;
     private int quantity;
     private double price;
@@ -16,5 +15,19 @@ class CartItem {
         return name;
     }
 
-}
+    public int getQuantity() {
+        return quantity;
+    }
 
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
